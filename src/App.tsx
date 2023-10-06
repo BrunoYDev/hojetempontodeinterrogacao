afeadc4390472732eeaa0398a3378efef954d0c1
Bruno Rodrigues Garcia <bruno.rogues.garcia@gmail.com>
2023-10-05 22:44:03 -0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import MovingButton from "./components/movingButton"
+function App() {
+
+
+  return (
+    <>
+      <h1>Hoje Tem?</h1>
+      <div>
+      <a href="https://i.pinimg.com/1200x/7d/3c/d6/7d3cd65da2182b47531a82c7391c97c0.jpg"><button>Sim</button></a>
+      <MovingButton />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/movingButton.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const MovingButton = () => {
+const [top,setTop] = useState(0);
+const [left,setLeft] = useState(0);
+
+const handleClick = () => {
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+
+    // Gerar coordenadas aleatórias dentro da janela do navegador
+    const newTop = Math.random() * (windowHeight - 50); // Subtrai 50 para manter o botão visível na tela
+    const newLeft = Math.random() * (windowWidth - 100); // Subtrai 100 para manter o botão visível na tela
+
+    setTop(newTop);
+    setLeft(newLeft);
+  };
+
+  const buttonStyle: React.CSSProperties = {
+    position: 'relative',
+    top: `${top}px`,
+    left: `${left}px`,
+  };
+
+    return (
+        <button style={buttonStyle} onClick={() => handleClick()}>
+        Não
+      </button>
+    )
+}
+
+export default MovingButton;
